68348e1eed8446f0fa6a793d15303b448cb453f9
zVharoldVz <harold_54430@hotmail.com>
2026-04-17 13:10:53 -0500

    Update settings.py
    elimar el secret key

--- Impomax/settings.py
+++ Impomax/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pm7a6jifq^##lu_ow@xo4+2@2v6-0_qhaw=a7gk@9n82ilfevz'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
